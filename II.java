6acd0aa2288c4f0c1e3044e9fe8e451bd486ec7e
shakilakarthi <shakilakarthi96@gmail.com>
2016-07-16 18:33:19 +0530

    Add files via upload
    Palindrome

---
+++ II.java
@@ -0,0 +1,28 @@
+
+public class II {
+
+	
+	public static void main(String[] args) {
+		
+		{
+			int n=525,n1,rev=0,rem;
+		
+			n1=n;
+			while(n>0)
+			{
+				rem=n%10;
+				rev=rev*10+rem;
+				n=n/10;
+			}
+			if(n1==rev)
+			{
+				System.out.println(+n1+ " is Palindrome");
+			}
+			else
+			{
+				System.out.println(+n1+ " is not Palindrome");
+			}
+	}
+
+	}
+}
